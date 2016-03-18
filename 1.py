e9a0e976984fe3cfc5eff7958eeb82162e4ca641
tanya9779 <tanya9779@mail.ru>
2016-03-18 10:37:17 +0300

    load from map and visualize

--- 1.py
+++ 1.py
@@ -1,36 +1,25 @@
 import matplotlib.pyplot as plt
-
 import networkx as nx
+import codecs
 
+def LoadGraph(f_name): # из файла прочитаем ребра и их вес
+    with codecs.open (f_name, 'r', 'utf-8') as f :
+        for line in f.read().split('\n'):
+            u,v,w = line.split()
+            w = float(w)
+            labels[u]='$'+u+'$'
+            labels[v]='$'+v+'$'
+            G.add_edge(u,v,weight=w/100)
+
+
+#----- main --------
 G = nx.Graph()
-
-# some math labels
 labels={}
+LoadGraph('map.txt')
+pos=nx.spring_layout(G,weight='weight') # positions for all nodes
 
-G.add_node(0, x=0, y=0)
-labels[0]=r'$Москва$'
-G.add_node(1, x=-5, y=2)
-labels[1]=r'$Санкт-Петербург$'
-G.add_node(2, x=5, y=0)
-labels[2]=r'$Казань$'
-G.add_node(3, x=10, y=-2)
-labels[3]=r'$Уфа$'
-G.add_node(4, x=6, y=-5)
-labels[4]=r'$Пермь$'
-
-G.add_edge(0,1)
-G.add_edge(0,2)
-G.add_edge(2,3)
-G.add_edge(3,4)
-G.add_edge(4,2)
-G.add_edge(4,3)
-G.add_edge(0,4)
-
-pos=nx.spring_layout(G) # positions for all nodes
-
-nx.draw_networkx_edges(G,pos,width=1.0,alpha=0.5)
-
-nx.draw_networkx_labels(G,pos,labels,font_size=16)
+nx.draw_networkx_edges(G,pos,alpha=0.5)
+nx.draw_networkx_labels(G,pos,labels,font_size=10)
 
 plt.axis('off')
 plt.savefig("labels_and_colors.png") # save as png
